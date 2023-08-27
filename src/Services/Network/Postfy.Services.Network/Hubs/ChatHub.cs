d1e93f93b2f22ef02e99f91e5a184825e584927a
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2023-08-27 20:40:33 +0300

    added signalr

---
+++ src/Services/Network/Postfy.Services.Network/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.SignalR;
+using Postfy.Services.Network.Messages.Dtos;
+using Postfy.Services.Network.Messages.Features.CreatingMessage.v1;
+
+namespace Postfy.Services.Network.Hubs;
+
+public class ChatHub : Hub
+{
+    private readonly ISender _sender;
+
+    public ChatHub(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    public async Task SendMessageAsync(string chatId, MessageBriefDto dto)
+    {
+        dto.IsWrittenByYou = false;
+        await Clients.OthersInGroup(chatId).SendAsync("ReceiveMessage", dto);
+    }
+
+    public async Task JoinToChat(string chatId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, chatId);
+    }
+
+    public async Task LeaveChat(string chatId)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatId);
+    }
+}
--- src/Services/Network/Postfy.Services.Network/Messages/Features/CreatingMessage/v1/CreateMessage.cs
+++ src/Services/Network/Postfy.Services.Network/Messages/Features/CreatingMessage/v1/CreateMessage.cs
@@ -1,9 +1,13 @@
 using Ardalis.GuardClauses;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using BuildingBlocks.Abstractions.CQRS.Commands;
 using BuildingBlocks.Security.Extensions;
 using BuildingBlocks.Security.Jwt;
+using Microsoft.EntityFrameworkCore;
 using Postfy.Services.Network.Chats;
 using Postfy.Services.Network.Comments.Features.GettingCommentsByPostId.v1;
+using Postfy.Services.Network.Messages.Dtos;
 using Postfy.Services.Network.Messages.Models;
 using Postfy.Services.Network.Shared.Contracts;
 
@@ -16,11 +20,17 @@ public class CreateMessageHandler : ICommandHandler<CreateMessage, CreateMessage
 {
     private readonly INetworkDbContext _context;
     private readonly ISecurityContextAccessor _securityContextAccessor;
+    private readonly IMapper _mapper;
 
-    public CreateMessageHandler(INetworkDbContext context, ISecurityContextAccessor securityContextAccessor)
+    public CreateMessageHandler(
+        INetworkDbContext context,
+        ISecurityContextAccessor securityContextAccessor,
+        IMapper mapper
+    )
     {
         _context = context;
         _securityContextAccessor = securityContextAccessor;
+        _mapper = mapper;
     }
 
     public async Task<CreateMessageResponse> Handle(CreateMessage request, CancellationToken cancellationToken)
@@ -39,6 +49,11 @@ public class CreateMessageHandler : ICommandHandler<CreateMessage, CreateMessage
         await _context.Messages.AddAsync(message, cancellationToken);
         await _context.SaveChangesAsync(cancellationToken);
 
-        return new CreateMessageResponse(message);
+        var dto = await _context.Messages
+                      .Include(x => x.Sender)
+                      .Include(x => x.Post)
+                      .ProjectTo<MessageBriefDto>(_mapper.ConfigurationProvider, new {currentUserId = userId})
+                      .FirstAsync(x => x.Id == message.Id, cancellationToken: cancellationToken);
+        return new CreateMessageResponse(dto);
     }
 }
