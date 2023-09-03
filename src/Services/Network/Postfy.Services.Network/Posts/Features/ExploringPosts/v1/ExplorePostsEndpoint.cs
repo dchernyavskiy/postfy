9fa5d050b8c13409c15830dee56335849b5247db
dchernyavskiy <99835818+dchernyavskiy@users.noreply.github.com>
2023-09-03 20:24:27 +0300

    added explore query

---
+++ src/Services/Network/Postfy.Services.Network/Posts/Features/ExploringPosts/v1/ExplorePostsEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using Ardalis.ApiEndpoints;
+using Asp.Versioning;
+using BuildingBlocks.Abstractions.CQRS.Queries;
+using Hellang.Middleware.ProblemDetails;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Postfy.Services.Network.Posts.Features.ExploringPosts.v1;
+
+public class ExplorePostsEndpoint : EndpointBaseAsync.WithRequest<ExplorePosts>.WithActionResult<ExplorePostsResponse>
+{
+    private readonly IQueryProcessor _queryProcessor;
+
+    public ExplorePostsEndpoint(IQueryProcessor queryProcessor)
+    {
+        _queryProcessor = queryProcessor;
+    }
+
+    [HttpGet(PostConfigs.PrefixUri + "/explore", Name = "ExplorePosts")]
+    [ProducesResponseType(typeof(ExplorePostsResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(StatusCodeProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(StatusCodeProblemDetails), StatusCodes.Status400BadRequest)]
+    [SwaggerOperation(
+        Summary = "ExplorePosts",
+        Description = "ExplorePosts",
+        OperationId = "ExplorePosts",
+        Tags = new[]
+               {
+                   PostConfigs.Tag
+               })]
+    [ApiVersion(1.0)]
+    public override async Task<ActionResult<ExplorePostsResponse>> HandleAsync(
+        [FromQuery] ExplorePosts request,
+        CancellationToken cancellationToken = new CancellationToken()
+    )
+    {
+        return await _queryProcessor.SendAsync(request, cancellationToken);
+    }
+}
---
+++ src/Services/Network/Postfy.Services.Network/Posts/Features/ExploringPosts/v1/ExplorePostsResponse.cs
@@ -0,0 +1,6 @@
+﻿using BuildingBlocks.Core.CQRS.Queries;
+using Postfy.Services.Network.Posts.Dtos;
+
+namespace Postfy.Services.Network.Posts.Features.ExploringPosts.v1;
+
+public record ExplorePostsResponse(ListResultModel<PostBriefDto> Body, Guid LastPostId);
